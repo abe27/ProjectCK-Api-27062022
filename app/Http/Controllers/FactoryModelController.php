6c601aed714873fd7306842cd7e36706c2c4a3d4
Parzival Yuenyang <krumii.it@gmail.com>
2022-06-27 09:49:55 +0700

    first init

---
+++ app/Http/Controllers/FactoryModelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FactoryModel;
+use Illuminate\Http\Request;
+
+class FactoryModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FactoryModel  $factoryModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FactoryModel $factoryModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FactoryModel  $factoryModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FactoryModel $factoryModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FactoryModel  $factoryModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FactoryModel $factoryModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FactoryModel  $factoryModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FactoryModel $factoryModel)
+    {
+        //
+    }
+}
---
+++ app/Traits/Nanoids.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Traits;
+
+use Hidehalo\Nanoid\Client;
+
+trait Nanoids
+{
+    /**
+     * Boot function from Laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $client = new Client();
+                $model->{$model->getKeyName()} = $client->generateId($size = 36, $mode = Client::MODE_DYNAMIC);
+            }
+        });
+    }
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+    /**
+     * Get the auto-incrementing key type.
+     *
+     * @return string
+     */
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
